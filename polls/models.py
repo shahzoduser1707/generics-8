fff8b898c8afd395d2f59f3be20844d4c30a11ec
Shahzod <tohirovshahzod1707@gmail.com>
2023-09-18 02:28:19 +0500

    only get permission added

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -10,6 +11,7 @@ class MusicModel(models.Model):
     price = models.IntegerField(default=0)
     created_at = models.DateTimeField(default=datetime.now)
     desc = models.TextField(default='Do you like music?')
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
 
 
     class Meta:
--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,13 @@ from rest_framework.response import Response
 from .serializers import MusicSerializers
 from .models import MusicModel
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class GetMusic(generics.ListAPIView):
       queryset = MusicModel.objects.all()
       serializer_class = MusicSerializers
+      permission_classes = (IsAuthenticated,)
 
 class CreateMusic(generics.CreateAPIView):
       queryset = MusicModel.objects.all()
